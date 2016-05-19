385f3bd8b5370851b4fe9b048e9efa82cc891f65
Nick Ayers <n1ckay3r5@gmail.com>
2016-05-19 01:20:32 +0100

    added dial

--- client.js
+++ client.js
@@ -1,27 +1,61 @@
 var socket;
+var val=0;
+var onelap=500.0;
+var div=360/onelap;
+var width=800, height=500;
 
 connect();
 
-text=d3.select("body").append("p").text("-");
-dial=d3.select("body").append("svg").
+dial=d3.select("body").append("svg").attr("width",width).attr("height",height).
 	append("circle").
-	attr("r",50).attr("cx",50).attr("cy",50);
-dial.classed('dial',true);
+	attr("r",190).
+	attr("cx",400).
+	attr("cy",200).
+	classed('dial',true);
+hand=d3.select("svg").append("line").
+	attr("x1",400).attr("y1",60).
+	attr("x2",400).attr("y2",10).
+	classed("hand",true);
+d3.select("svg").append("text").attr("x",310).attr("y",200).classed("timer","true");
+
+function updateHand(data) {
+	d3.select(".hand").data([data]).attr("transform",function(d) {
+		return "rotate("+div*(d%onelap)+",400,200)";
+	});
+}
 
 function connect() {
-	try {
-		socket=new WebSocket("ws://localhost:8081");
-		socket.onopen=openSocket;
-		socket.onmessage=showMessage;
-	} catch (e) {
-		setTimeout(connect,500);
-	}
+	socket=new WebSocket("ws://localhost:8081");
+	console.log("connected");
+	socket.onclose=function(m) {
+		setTimeout(connect,500)
+	};
+	socket.onopen=openSocket;
+	socket.onmessage=showMessage;
 }
 
 function openSocket() {
 	text.text("0");
 }
 
+function formatTwoDigits(num) {
+	if (num<10) {
+		return "0"+num;
+	} else {
+		return ""+num;
+	}
+}
+
+function formatTime(millis) {
+	secs=parseInt(millis/1000);
+	mins=parseInt(secs/60);
+	return formatTwoDigits(mins)+":"+formatTwoDigits((secs%60))+":"+(millis%1000);
+}
+
 function showMessage(message) {
-	text.text(message.data);
+	data=message.data.split(',');
+	dist=parseInt(data[0]);
+	millis=formatTime(data[1]);
+	d3.select(".timer").text(millis);
+	updateHand([dist]);
 }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var WebSocketServer=require("ws").Server;
 var wss=new WebSocketServer({port: 8081});
 var connections=new Array;
 var t=0;
+var starttime=Date.now();
 
 wss.on('connection', handleConnection);
 
@@ -15,14 +16,16 @@ function handleConnection(client) {
 setTimeout(tick,500);
 
 function tick() {
-	broadcast(t++);
-	setTimeout(tick,500);
+	msg=(t+=5)+","+(Date.now()-starttime);
+	broadcast(msg);
+	setTimeout(tick,50);
 }
 
 function broadcast(data) {
 	for (c in connections) {
 		connections[c].send(data+"",function ack(error) {
 			if (error) {
+				console.log("client diconnect");
 				connections.splice(c,1);
 			}
 		});
